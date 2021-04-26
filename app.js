e493fcf37a4c18022b79d3d81e65c5cd72c57b20
berat <cedinckan@gmail.com>
2021-04-26 12:01:20 +0300

    negative and postive real data reading

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const net = require('net');
 const socket = new net.Socket();
 
 const options = {
-  'host':'192.168.0.67',
+  'host':'192.168.0.68',
   'port':'502'
 }
 
@@ -19,19 +19,64 @@ socket.on('connect',()=>{
   //   console.log(resp.response)
   // })
   client.readHoldingRegisters(2,2).then((resp)=>{
+    //UINT32 READING
+    //console.log(resp.response.body);
+    const testData =resp.response.body.values;
+    const testData1 = resp.response.body.valuesAsArray;
+    const uint32Data = (resp.response.body.values[0])+(resp.response.body.values[1]<<16); //as uint32data
+    const newBuffer = Buffer.from(resp.response.body.valuesAsBuffer);
+    
+    //console.log(uint32Data);
+    //bufferData = resp.response.body.valuesAsBuffer.
+    //const bufferData1 = resp.response.body.valuesAsBuffer.readUInt32BE(1);
+    
+    //console.log('testData :' +testData);
+    //console.log('testData1 :'+testData1)
+    
+    //console.log(uint16ToFloat32(54464,1));
+    
+
+    
+  }).catch(error=>{
+    //console.log(error);
+  })
+  client.readHoldingRegisters(4,2).then((resp)=>{
+    // REAL DATA READING positive and negative 
     console.log(resp.response.body);
     const testData =resp.response.body.values;
     const testData1 = resp.response.body.valuesAsArray;
-    const bufferData = resp.response.body.valuesAsBuffer;
-    const newBuffer = Buffer.from(resp.response.body.valuesAsBuffer);
-    const newData =new Int32Array(newBuffer);
-    console.log(newData);
+    //const uint32Data = (resp.response.body.values[0])+(resp.response.body.values[1]<<16);
+    // const newBuffer = Buffer.from(resp.response.body.valuesAsBuffer);
     
+    //console.log(uint32Data);
     //bufferData = resp.response.body.valuesAsBuffer.
     //const bufferData1 = resp.response.body.valuesAsBuffer.readUInt32BE(1);
     
     console.log('testData :' +testData);
     console.log('testData1 :'+testData1)
+    const buf = Buffer.allocUnsafe(4);
+    
+    buf.writeUInt16BE(resp.response.body.values[0],2);
+    buf.writeUInt16BE(resp.response.body.values[1],0);
+    const newBuf = buf.readFloatBE(0);
+    console.log('newBuff :' +newBuf);
+    //console.log(uint16ToFloat32(54464,1));
+    
+
+    
+  }).catch(error=>{
+    //console.log(error);
+  })
+  client.readHoldingRegisters(4,2).then((resp)=>{
+    //reading 16 bit data
+    console.log(resp.response.body);
+    const testData =resp.response.body.values;
+    const testData1 = resp.response.body.valuesAsArray;
+    
+    console.log('testData :' +testData);
+    console.log('testData1 :'+testData1)
+    const buf = Buffer.allocUnsafe(4);
+    
     
     //console.log(uint16ToFloat32(54464,1));
     
@@ -46,6 +91,7 @@ socket.on('connect',()=>{
 
   // })
 })
+
 
 function uint16ToFloat32(low, high) {
   var buffer = new ArrayBuffer(4);
